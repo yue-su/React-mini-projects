99470670f989b7677b980874318076c2792713b2
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-08-30 16:14:19 -0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import '../styles/App.css';
+import {Switch} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+
+      <Switch>
+        
+      </Switch>
+
     </div>
   );
 }
