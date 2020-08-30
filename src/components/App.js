bb033092968984ed069e8c54a9f6cf0665a319ae
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-08-30 16:47:15 -0300

    added Link

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../styles/App.css';
-import {Switch} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import MovieSeatBooking from './movie/MovieSeatBooking';
+import Home from './Home';
+
 
 function App() {
   return (
     <div className="App">
 
       <Switch>
-        
+        <Route path ='/' component={Home}  exact />
+        <Route path='/movie' component={MovieSeatBooking} />
       </Switch>
 
     </div>
---
+++ src/components/movie/MovieSeatBooking.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+const MovieSeatBooking = () => {
+    return (
+        <div>
+            <Typography variant='h2'>Movie Seat Booking</Typography>
+        </div>
+    )
+}
+
+export default MovieSeatBooking
