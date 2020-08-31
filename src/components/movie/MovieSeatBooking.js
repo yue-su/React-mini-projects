6db084a68da719e6c66ecd3f254dc8799778c946
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-08-30 22:49:22 -0300

    added select

--- src/components/movie/MovieSeatBooking.js
+++ src/components/movie/MovieSeatBooking.js
@@ -1,10 +1,40 @@
-import React from 'react'
-import { Typography } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Typography, Container, FormControl, InputLabel, Select, MenuItem, Box } from '@material-ui/core'
+import EventSeatIcon from "@material-ui/icons/EventSeat"
+
+const initialPrice = 0
+const initialSeats = []
 
 const MovieSeatBooking = () => {
+
+    const [price, setPrice] = useState(initialPrice)
+    
+    const handleChange = event => {
+        const { value } = event.target
+        setPrice(value)
+    }
+
     return (
         <div>
-            <Typography variant='h2'>Movie Seat Booking</Typography>
+            <Typography variant='h2' align='center'>Movie Seat Booking</Typography>
+            <Container>
+                <FormControl>
+                    <InputLabel id='ticket price'>Price</InputLabel>
+                    <Select
+                        labelId='ticket price'
+                        name='price'
+                        onChange={handleChange}
+                    >
+                        <MenuItem value={10}>$10</MenuItem>
+                        <MenuItem value={15}>$15</MenuItem>
+                        <MenuItem value={20}>$20</MenuItem>   
+                    </Select>
+                </FormControl>
+                <Box>
+                    <Typography>{price}</Typography>
+
+                </Box>
+            </Container>
         </div>
     )
 }
