4aaf848a3b79a8aa083f48310326c32971bcdc8e
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-06 19:46:51 -0300

    finished home page retouch

--- src/components/Home/AuthorCard.js
+++ src/components/Home/AuthorCard.js
@@ -6,22 +6,27 @@ import {
   
   Grid,
   Avatar,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
   
 } from "@material-ui/core"
 import avatarRyan from "../../images/Avatar_Ryan.jpeg"
 import avatarYue from "../../images/Avatar_Yue.JPG"
 import { makeStyles } from "@material-ui/core/styles"
+import EmailIcon from "@material-ui/icons/Email"
+import LanguageIcon from "@material-ui/icons/Language"
+import PermIdentityIcon from "@material-ui/icons/PermIdentity"
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
 
 const useStyles = makeStyles((theme) => ({
-  media: {
-    height: 240,
-  },
   large: {
     width: theme.spacing(10),
     height: theme.spacing(10),
   },
   author: {
-    height: "50%",
+        marginBottom: '10px',
       backgroundColor: theme.palette.grey[100],
     borderRadius: '10px'
   },
@@ -32,44 +37,131 @@ const AuthorCard = () => {
     const classes = useStyles()
 
     return (
-      <Grid sm={6} item container direction="column" spacing={1} justify='space-between'>
-        <Grid
+      <Grid
+            md={6}
+            xs={12}
+        item
+        container
+        spacing={1}
+        justify="space-between"
+      >
+            <Grid
           item
           container
           direction="column"
           className={classes.author}
           spacing={1}
         >
-          <Grid item>
-            <Avatar src={avatarYue} className={classes.large}></Avatar>
+          <Grid item container spacing={2} alignItems="center">
+            <Grid item>
+              <Avatar src={avatarYue} className={classes.large}></Avatar>
+            </Grid>
+            <Grid item>
+              <Typography gutterBottom variant="h6">
+                Yue Su
+              </Typography>
+              <Typography>Full-Stack Developer</Typography>
+            </Grid>
           </Grid>
           <Grid item>
-            <Typography gutterBottom variant="h6">
-              Yue Su
-            </Typography>
-            <Typography gutterBottom>Full-Stack Developer</Typography>
+            <List dense>
+              <ListItem >
+                <ListItemIcon>
+                  <PermIdentityIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography>
+                    about me and about me and about me and about meand about
+                    meand about me and about me
+                  </Typography>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <LanguageIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography>www.yue-su.dev</Typography>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <EmailIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography>su2047@gmail.com</Typography>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <LinkedInIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography>linkedin.com/in/suxyue/</Typography>
+                </ListItemText>
+              </ListItem>
+            </List>
           </Grid>
-          <Grid item>Contract</Grid>
         </Grid>
-        <Grid
+            <Grid
           item
           container
           direction="column"
           className={classes.author}
           spacing={1}
         >
-          <Grid item>
-            <Avatar src={avatarRyan} className={classes.large}></Avatar>
+          <Grid item container spacing={2} alignItems="center">
+            <Grid item>
+              <Avatar src={avatarRyan} className={classes.large}></Avatar>
+            </Grid>
+            <Grid item>
+              <Typography gutterBottom variant="h6">
+                Ryan Lee
+              </Typography>
+              <Typography>Full-Stack Developer</Typography>
+            </Grid>
           </Grid>
           <Grid item>
-            <Typography gutterBottom variant="h6">
-              Ryan Lee
-            </Typography>
-            <Typography gutterBottom>Full-Stack Developer</Typography>
+            <List dense>
+              <ListItem >
+                <ListItemIcon>
+                  <PermIdentityIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography>
+                    about me and about me and about me and about meand about
+                    meand about me and about me
+                  </Typography>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <LanguageIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography>www.yue-su.dev</Typography>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <EmailIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography>su2047@gmail.com</Typography>
+                </ListItemText>
+              </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  <LinkedInIcon />
+                </ListItemIcon>
+                <ListItemText>
+                  <Typography>linkedin.com/in/suxyue/</Typography>
+                </ListItemText>
+              </ListItem>
+            </List>
           </Grid>
-          <Grid item>Contract</Grid>
         </Grid>
-        
+            
       </Grid>
     )
 }
--- src/components/Home/Introduction.js
+++ src/components/Home/Introduction.js
@@ -1,38 +1,56 @@
 import React from "react"
-import { Typography, Grid } from "@material-ui/core"
+import { Typography, Grid, Link } from "@material-ui/core"
+import GitHubIcon from "@material-ui/icons/GitHub"
 
 const Introduction = () => {
   return (
-    <Grid sm={6} item>
+    <Grid md={6} xs={12} item>
       <Typography variant="h5" paragraph>
-        Introduction
+        Why mini projects ?
       </Typography>
       <Typography paragraph>
         As a front-end developer, it is imperative to keep sharpening your
-        skills, updating knowledge, and tapping into some unfamiliar territory
-        where you have never encountered. Although there already are tons of
-        tutorials, articles, books, talks or codepens, nothing could ever
-        compare to some real hands-on projects. Get your hands dirty, make
-        mistakes, break things, fix things, slowly buildup a sturdy
-        understanding of the hardcore concepts, and gain some real experiences.
+        skills, updating knowledge, and tapping into some unfamiliar
+        territories. Although there are tons of tutorials, articles, books,
+        talks or codepens, nothing could compare to real hands-on projects. It's
+        time to get your hands dirty, make mistakes, break things, fix things,
+        slowly buildup a sturdy understanding of the hardcore concepts, and gain
+        some real experiences.
       </Typography>
       <Typography paragraph>
-        This website is a personal project about implementing some of the key
-        concepts I learned when studying in Lambda school. It is a demonstration
-        of a series of 20 projects that are all made by React and other related
-        technology such as React-Router, Hooks, Material-UI, etc. These projects
-        are inspired by Brad Travery's Udemy course-- 20 Web Projects With
-        Vanilla JavaScript. Each project emphasizes different things, including
-        JavaScript fundamentals, DOM Manipulation, API & JSON, High Order Array
-        Methods and more.
+        This website is a project about implementing some of the key concepts we
+        learned when studying in{" "}
+        <Link variant="body1" href="https://lambdaschool.com/">
+          Lambda School
+        </Link>
+        . It is a demonstration of a series of projects that are all made by
+        React and other related technology such as React-Router, Hooks,
+        Material-UI, etc. These projects are inspired by{" "}
+        <Link variant="body1" href="https://twitter.com/traversymedia">
+          Brad Travery
+        </Link>
+        's Udemy course--{" "}
+        <Link
+          variant="body1"
+          href="https://www.udemy.com/course/web-projects-with-vanilla-javascript"
+        >
+          20 Web Projects With Vanilla JavaScript
+        </Link>
+        . Each project emphasizes different things, including JavaScript
+        fundamentals, DOM Manipulation, API & JSON, High Order Array Methods and
+        more.
       </Typography>
       <Typography paragraph>
-        I encourage anyone, especially junior developers interested in React, to
-        take on some of the projects. Write some code and compare what you did
-        with mine. Chances are you would do better, or you might have some
-        blockers as I did. Either way, we could all gain something in the end.
+        We encourage anyone, especially junior developers interested in React,
+        to take on some of the projects. Write some code and compare what you
+        did with our Github repo. Chances are you would do better, or you might
+        have some blockers. Either way, we could all gain something in the end.
       </Typography>
-      <Typography paragraph>Yue</Typography>
+      <Typography align='center'>
+        <Link href="https://github.com/yue-su/React-mini-projects">
+          <GitHubIcon />
+        </Link>
+      </Typography>
     </Grid>
   )
 }
