6e68cf8a9a6fc6d86822d32b1bddce76d1ff30aa
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-10-19 00:36:23 -0300

    updated expense tracker

--- src/components/ExpenseTracker/ExpenseTracker.js
+++ src/components/ExpenseTracker/ExpenseTracker.js
@@ -1,29 +1,71 @@
 import React from 'react'
-import {  Grid,  Box } from "@material-ui/core"
+import { Grid, Box, Typography, TextField } from "@material-ui/core"
 import Description from "./Description"
 
 const ExpenseTracker = () => {
     return (
-        <Grid container spacing={1}>
-            <Grid item md={6} xs={12}>
-                <Box
-                    display="flex"
-                    flexDirection="column"
-                    justifyContent="center"
-                    alignItems="center"
-                    border="2px solid purple"
-                    borderRadius="10px"
-                    padding="1rem"
-                >
-                    <Grid container direction="column" >
-                        <Grid item>
-                            
-                        </Grid>
-                    </Grid>
-        </Box>
+      <Grid container spacing={1}>
+        <Grid item md={6} xs={12}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            border="2px solid purple"
+            borderRadius="10px"
+            padding="1rem"
+          >
+            <Grid container direction="column" spacing={2}>
+              <Grid item>
+                <Typography variant="h6" align="center">
+                  Expense Tracker
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography variant="subtitle2">YOUR BALANCE:</Typography>
+                <Typography>$200</Typography>
+              </Grid>
+              <Grid item container>
+                <Grid item xs={6} align="center">
+                  <Typography>INCOME</Typography>
+                  <Typography>$300</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography>EXPENSE</Typography>
+                  <Typography>$100</Typography>
+                </Grid>
+              </Grid>
+              <Grid item>
+                <Typography>History</Typography>
+                <hr />
+                List place holder
+              </Grid>
+              <Grid item>
+                <Typography>Add new transaction</Typography>
+                <hr />
+                <form>
+                  <Typography>Text</Typography>
+                  <TextField
+                    id="outlined-basic"
+                    label="Enter text"
+                    variant="standard"
+                  />
+                  <Typography>Amount</Typography>
+                  <Typography>
+                    (negative - expense, positive - income)
+                  </Typography>
+                  <TextField
+                    id="outlined-basic"
+                    label="Enter amount"
+                    variant="standard"
+                  />
+                </form>
+              </Grid>
             </Grid>
-            <Description />
+          </Box>
         </Grid>
+        <Description />
+      </Grid>
     )
 }
 export default ExpenseTracker
