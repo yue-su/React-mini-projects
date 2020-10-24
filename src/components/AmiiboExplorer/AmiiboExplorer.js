fdff00f1fb497b785e43cb504749460646a4a8fd
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-10-23 22:50:29 -0300

    Update AmiiboExplorer.js

--- src/components/AmiiboExplorer/AmiiboExplorer.js
+++ src/components/AmiiboExplorer/AmiiboExplorer.js
@@ -1,12 +1,21 @@
-import React, { useEffect } from "react"
-import { Grid, Box, Typography, Button } from "@material-ui/core"
+import React, { useEffect, useState } from "react"
+import { Grid, Box, Typography, Button, Select, InputLabel } from "@material-ui/core"
 import Description from "./Description"
-
+import axios from "axios"
 
 
 const AmiiboExplorer = () => {
 
- 
+    const [series, setSeries] = useState([])
+    const [amiiboList, setAmiiboList] = useState([])
+
+    useEffect(() => {
+        axios
+            .get(`https://www.amiiboapi.com/api/amiiboseries`)
+            .then(res => {
+            setSeries(res.data.amiibo)
+        })
+    },[])
 
     return (
       
@@ -21,7 +30,22 @@ const AmiiboExplorer = () => {
               borderRadius="10px"
               padding="1rem"
             >
-              
+                    <Grid container direction='column'>
+                        <Grid item>
+                            <Typography variant='h6' align='center'>Amiibo Explorer</Typography>
+                        </Grid>
+                        <Grid item>
+                            <InputLabel id="serie">Choose a serie</InputLabel>
+                            <Select
+                                labelId='serie'
+                                
+                            
+                            >
+
+                            </Select>
+                        </Grid>
+
+              </Grid>
             </Box>
           </Grid>
           <Description />
