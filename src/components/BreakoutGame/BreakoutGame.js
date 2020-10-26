b6cb19980336e5584b2c4fe29f2468d7bc27ea8a
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-10-25 23:35:10 -0300

    added breakoutgame

---
+++ src/components/BreakoutGame/BreakoutGame.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Grid, Box } from "@material-ui/core"
+import Description from "./Description"
+
+const BreakoutGame = () => {
+    return (
+      <Grid container spacing={1}>
+        <Grid item md={6} xs={12}>
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            border="2px solid purple"
+            borderRadius="10px"
+            padding="1rem"
+          >
+            This is where to put the actual project JSX
+          </Box>
+        </Grid>
+        <Description />
+      </Grid>
+    )
+}
+
+export default BreakoutGame
