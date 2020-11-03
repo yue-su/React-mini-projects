6e5dc92909d835743c20d3de1a329955b0e3b9f6
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-11-02 22:24:18 -0400

    Update BreakoutGame.js
    added ball and paddle

--- src/components/BreakoutGame/BreakoutGame.js
+++ src/components/BreakoutGame/BreakoutGame.js
@@ -1,18 +1,74 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Grid, Box, Typography } from "@material-ui/core"
 import Description from "./Description"
 import "./BreakoutGame.css"
 
+const ballInit = {
+  x: 0,
+  y: 0,
+  size: 0,
+  speed: 0,
+  dx: 0,
+  dy: 0,
+}
+const paddleInit = {
+  x: 0,
+  y: 0,
+  w: 0,
+  h: 0,
+  speed: 0,
+  dx: 0,
+}
+
 const BreakoutGame = () => {
   const canvasRef = useRef(null)
 
+  const [ball, setBall] = useState(ballInit)
+  const [paddle, setPaddle] = useState(paddleInit)
+
+  const drawBall = (ctx, ball) => {
+    ctx.beginPath()
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2)
+    ctx.fillStyle = "#0095dd"
+    ctx.fill()
+    ctx.closePath()
+  }
+
+  const drawPaddle = (ctx, paddle) => {
+    ctx.beginPath()
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+    ctx.fillStyle = "#0095dd"
+    ctx.fill()
+    ctx.closePath()
+  }
+
+  //set up canvas with useRef
   useEffect(() => {
     const canvas = canvasRef.current
-    const context = canvas.getContext("2d")
+    const ctx = canvas.getContext("2d")
 
-    context.fillStyle = "green"
-    context.fillRect(10, 10, 150, 100)
-  }, [])
+    setBall({
+      x: canvas.width / 2,
+      y: canvas.height / 2,
+      size: 5,
+      speed: 4,
+      dx: 4,
+      dy: -4,
+      //nature movement of the y is going down, so in order to go up here the value is -4.
+    })
+
+    setPaddle({
+      x: canvas.width / 2 - 40,
+      y: canvas.height - 20,
+      w: 80,
+      h: 10,
+      speed: 8,
+      dx: 0,
+    })
+
+    drawBall(ctx, ball)
+    drawPaddle(ctx, paddle)
+  }, [ball, paddle])
 
   return (
     <Grid container spacing={1}>
@@ -26,7 +82,7 @@ const BreakoutGame = () => {
           borderRadius="10px"
           padding="1rem"
         >
-          <Grid container>
+          <Grid container direction="column">
             <Grid item>
               <Typography>Breakout Game</Typography>
             </Grid>
