ddabef88c67e744d37ead86b2836f6c84c83f0c0
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-11-09 21:40:22 -0400

    updated customvideoplayer's description

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ### Why mini projects
 
-This website is a project about implementing some of the key concepts I learned when studying in Lambda School. It is a demonstration of a series of projects that are all made by React and other related technology such as React-Router, Hooks, Material-UI, etc. These projects are inspired by Brad Travery's Udemy course-- 20 Web Projects With Vanilla JavaScript. Each project emphasizes different things, including JavaScript fundamentals, DOM Manipulation, API & JSON, High Order Array Methods and more.
+This website is a project about implementing some of the key concepts I learned when studying in Lambda School. It is a demonstration of a series of projects that are all made by React and other related technology such as React-Router, Hooks, Material-UI, etc.
+
+These projects are inspired by Brad Travery's Udemy course-- 20 Web Projects With Vanilla JavaScript. Each project emphasizes different things, including JavaScript fundamentals, DOM Manipulation, API & JSON, High Order Array Methods and more.
 
 ### UI
 
--- src/components/CustomVideoPlayer/Description.js
+++ src/components/CustomVideoPlayer/Description.js
@@ -1,37 +1,60 @@
 import React from "react"
-import { Typography, Grid, Box, Paper } from "@material-ui/core"
+import { Typography, Grid, Paper } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+import EmojiObjectsOutlinedIcon from "@material-ui/icons/EmojiObjectsOutlined"
+import GradeOutlinedIcon from "@material-ui/icons/GradeOutlined"
+
+const useStyles = makeStyles(() => ({
+  container: {
+    padding: "1.5rem",
+  },
+}))
 
 const Description = () => {
+  const classes = useStyles()
+
   return (
     <Grid item md={6} xs={12}>
-      <Paper>
-        <Box px={2}>
-          <Typography variant="h6" align="center" gutterBottom>
-            Custom Video Player
-          </Typography>
-          <Typography paragraph>
-            The tricky part is that you can not get the video length from the
-            video.duration(the html5 video/audio property) in the beginning when
-            the video is loaded. So, at first, when you try to get the length by
-            setting the related state to the number of seconds returned by
-            duration, you will get a NaN instead.
-          </Typography>
-          <Typography paragraph>There are two resolutions :</Typography>
-          <Typography paragraph>
-            One: Make a function to get the duration and setState. --> don't call
-            the function with useEffect() when the application is loaded,
-            instead, put the function in the play button's handler function. -->
-            User hit the play button, the duration function will be called. -->
-            get the actual length, and the state will be refreshed.
-          </Typography>
-          <Typography paragraph>
-            Two: Use an event called "onLoadMetaData" with a handler function.
-            --> get the duration from event.target --> setState and refresh the
-            length state. The two resolutions could be combined, so no matter
-            the video is auto-played or fired by the user, the actual length
-            could both get.
-          </Typography>
-        </Box>
+      <Paper className={classes.container}>
+        <Grid container spacing={1} direction="column">
+          <Grid item>
+            <Typography variant="subtitle1">
+              <GradeOutlinedIcon />
+              Features:
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography>
+              • The video player utilizes HTML5 meida API for a customized play,
+              pause, replay user interface
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="subtitle1">
+              <EmojiObjectsOutlinedIcon />
+              Height:
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography>
+              • The tricky part is that you can not get the video length by
+              video.duration(the html5 video/audio property) in the beginning
+              when the video is loaded. You will get a NaN instead.
+            </Typography>
+            <Typography>
+              • A solution for setting the actual length of the video is to put
+              an function in the play button's handler. when the user hit the
+              button, the duration function will be called and get the actual
+              length.
+            </Typography>
+            <Typography>
+              • an alternative solution is to set up an event called
+              onLoadMetaData with a handler function. You can get the duration
+              from destructing event.target object. This solution is suitable
+              for auto-played interface.
+            </Typography>
+          </Grid>
+        </Grid>
       </Paper>
     </Grid>
   )
