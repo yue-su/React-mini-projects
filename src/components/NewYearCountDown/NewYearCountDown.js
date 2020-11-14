67a9e0dcbc50fe932c11dac8a6b0727370058671
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-11-13 20:18:43 -0400

    Update NewYearCountDown.js

--- src/components/NewYearCountDown/NewYearCountDown.js
+++ src/components/NewYearCountDown/NewYearCountDown.js
@@ -1,21 +1,36 @@
-import React from "react"
-import { Grid, Box } from "@material-ui/core"
+import React, { useState } from "react"
+import { Grid, Box, Typography } from "@material-ui/core"
 import Description from "./Description"
 
 const NewYearCountDown = () => {
+  const [year, setYear] = useState("")
+
+  const [countdown, setCountdown] = useState({
+    days: "00",
+    hours: "00",
+    minutes: "00",
+    seconds: "00",
+  })
+
   return (
     <Grid container spacing={1}>
       <Grid item md={6} xs={12}>
         <Box
           display="flex"
           flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
           border="2px solid purple"
           borderRadius="10px"
           padding="1rem"
         >
-          This is where to put the actual project JSX
+          <Grid container direction="column">
+            <Grid item>
+              <Typography variant="h6">New Year Countdown</Typography>
+            </Grid>
+            <Grid item>
+              <Typography>{year}</Typography>
+            </Grid>
+            <Grid item></Grid>
+          </Grid>
         </Box>
       </Grid>
       <Description />
